1dfd78210c337d1ca22fb0c4fcb8eac68760dd97
seagalputra <seagalputra@gmail.com>
2025-03-11 22:22:40 +0700

    refactor: update Vite configuration for improved readability and add 'debug' to optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,33 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { getLoadContext } from "./load-context";
+import { reactRouter } from '@react-router/dev/vite'
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { getLoadContext } from './load-context'
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     rollupOptions: isSsrBuild
       ? {
-          input: "./workers/app.ts",
+          input: './workers/app.ts',
         }
       : undefined,
   },
   ssr: {
-    target: "webworker",
+    target: 'webworker',
     noExternal: true,
     resolve: {
-      conditions: ["workerd", "browser"],
+      conditions: ['workerd', 'browser'],
     },
     optimizeDeps: {
       include: [
-        "react",
-        "react/jsx-runtime",
-        "react/jsx-dev-runtime",
-        "react-dom",
-        "react-dom/server",
-        "react-router",
+        'react',
+        'react/jsx-runtime',
+        'react/jsx-dev-runtime',
+        'react-dom',
+        'react-dom/server',
+        'react-router',
+        'debug',
       ],
     },
   },
@@ -38,4 +39,4 @@ export default defineConfig(({ isSsrBuild }) => ({
     reactRouter(),
     tsconfigPaths(),
   ],
-}));
+}))
