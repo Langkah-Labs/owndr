f406ba208df9230fda278011fd66939a73026580
rizkyramadika <rizky@liquidx.studio>
2025-05-28 14:33:21 +0700

    migrate profile page components and ui

--- app/routes.ts
+++ app/routes.ts
@@ -16,5 +16,7 @@ export default [
 
   route('landing', './landing/layout.tsx', [index('./landing/index.tsx')]),
 
+  route('profile', './profile/layout.tsx', [index('./profile/index.tsx')]),
+
   route('signout', './signout/index.tsx'),
 ] satisfies RouteConfig
