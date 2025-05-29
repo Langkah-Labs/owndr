cfe0143e9115ed5c8773dc51ebf8edeacba286ae
rizkyramadika <rizky@liquidx.studio>
2025-05-29 19:58:56 +0700

    migration of about page and components

--- app/routes.ts
+++ app/routes.ts
@@ -16,5 +16,7 @@ export default [
 
   route('landing', './landing/layout.tsx', [index('./landing/index.tsx')]),
 
+  route('about', './about/layout.tsx', [index('./about/index.tsx')]),
+
   route('signout', './signout/index.tsx'),
 ] satisfies RouteConfig
